0da0f1c3fbfd750c68b0acf90803c13c4bf15e17
Jayakrishan Jayabal <jayakrishnan.jayabal@acquia.com>
2016-06-18 05:11:53 +0000

    Change to Drush driver.

---
+++ drush/local.drushrc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * #file
+ * Local drush configuration for this project.
+ */
+
+ $options['uri'] = 'http://bcd7.dev';
---
+++ drush/site-aliases/local.aliases.drushrc.php
@@ -0,0 +1,19 @@
+<?php
+
+// Use the folloing alias as a guide for creating a local alias.
+
+// $aliases['[PROJECT-NAME]'] = array(
+//   'uri' => '[PROJECT-NAME].dev',
+//   'root' => '/Users/[YOUR USERNAME]/Sites/[PROJECT-NAME]/docroot',
+//   'path-aliases' => array(
+//     '%dump-dir' => '/tmp',
+//   ),
+// );
+
+$aliases['bcd7'] = array(
+  'uri' => 'bcd7.dev',
+  'root' => '/var/www/drupalvm/bcd7/html',
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+  ),
+);
